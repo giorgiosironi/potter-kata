5c583a41017ca1b459579d13d47281f0a36a2b6a
Giorgio Sironi <giorgio.sironi@neomobile.com>
2013-11-01 13:54:56 +0100

    Updated tests to reflect in the API the possibility of choosing different titles

--- CartTest.php
+++ CartTest.php
@@ -14,41 +14,41 @@ class CartTest extends \PHPUnit_Framework_TestCase
 
     public function test1BookInTheCart()
     {
-        $this->cart->addBooks(1);
+        $this->cart->addBooks('A');
         $this->assertEquals(8, $this->cart->price());
     }
 
     public function test2DifferentBooksInTheCart()
     {
-        $this->cart->addBooks(1);
-        $this->cart->addBooks(1);
+        $this->cart->addBooks('A');
+        $this->cart->addBooks('B');
         $this->assertEquals(8 * 2 * 0.95, $this->cart->price());
     }
 
     public function test3DifferentBooksInTheCart()
     {
-        $this->cart->addBooks(1);
-        $this->cart->addBooks(1);
-        $this->cart->addBooks(1);
+        $this->cart->addBooks('A');
+        $this->cart->addBooks('B');
+        $this->cart->addBooks('C');
         $this->assertEquals(8 * 3 * 0.90, $this->cart->price());
     }
 
     public function test4DifferentBooksInTheCart()
     {
-        $this->cart->addBooks(1);
-        $this->cart->addBooks(1);
-        $this->cart->addBooks(1);
-        $this->cart->addBooks(1);
+        $this->cart->addBooks('A');
+        $this->cart->addBooks('B');
+        $this->cart->addBooks('C');
+        $this->cart->addBooks('D');
         $this->assertEquals(8 * 4 * 0.80, $this->cart->price());
     }
 
     public function test5DifferentBooksInTheCart()
     {
-        $this->cart->addBooks(1);
-        $this->cart->addBooks(1);
-        $this->cart->addBooks(1);
-        $this->cart->addBooks(1);
-        $this->cart->addBooks(1);
+        $this->cart->addBooks('A');
+        $this->cart->addBooks('B');
+        $this->cart->addBooks('C');
+        $this->cart->addBooks('D');
+        $this->cart->addBooks('E');
         $this->assertEquals(8 * 5 * 0.75, $this->cart->price());
     }
 }
@@ -64,7 +64,7 @@ class Cart
         5 => 0.25,
     ];
 
-    public function addBooks($number)
+    public function addBooks($title, $number = 1)
     {
         $this->books += $number;
     }    
