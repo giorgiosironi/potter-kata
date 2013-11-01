02abdcb6c6f5354fe2356df323f19a17864e016f
Giorgio Sironi <giorgio.sironi@neomobile.com>
2013-11-01 13:39:57 +0100

    Scenario: 1 book

--- CartTest.php
+++ CartTest.php
@@ -7,12 +7,26 @@ class CartTest extends \PHPUnit_Framework_TestCase
         $cart = new Cart();
         $this->assertEquals(0, $cart->price());
     }
+
+    public function test1BookInTheCart()
+    {
+        $cart = new Cart();
+        $cart->addBooks(1);
+        $this->assertEquals(8, $cart->price());
+    }
 }
 
 class Cart
 {
+    private $books = 0;
+
+    public function addBooks($number)
+    {
+        $this->books += $number;
+    }    
+
     public function price()
     {
-        return 0;
+        return 8 * $this->books;
     }
 }
