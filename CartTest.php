cec61610139d1148b45e3e4ef6ac3e37618fb014
Giorgio Sironi <giorgio.sironi@neomobile.com>
2013-11-02 10:14:06 +0100

    Retrieved Greedy extraction functionality

--- CartTest.php
+++ CartTest.php
@@ -104,6 +104,32 @@ class CartTest extends \PHPUnit_Framework_TestCase
 
     // -- Bundle unit tests
 
+    public function testABundleCalculateAPriceOnlyOnDifferentBooks()
+    {
+        list ($bundle, $remainingBooks) = Bundle::extractGreedily(['A' => 1, 'B' => 1]);
+        $this->assertEquals(8 * 2 * 0.95, $bundle->price());
+    }
+
+    public function testABundleCalculateAPriceOnAnyNumberOfDifferentBooks()
+    {
+        list ($bundle, $remainingBooks) = Bundle::extractGreedily(['A' => 1, 'B' => 1, 'C' => 1, 'D' => 1, 'E' => 1]);
+        $this->assertEquals(8 * 5 * 0.75, $bundle->price());
+    }
+
+    public function testABundleCanBeExtractedFromAGroupContainingTwoIdenticalBook()
+    {
+        list ($bundle, $remainingBooks) = Bundle::extractGreedily(['A' => 2, 'B' => 1]);
+        $this->assertEquals(new Bundle(['A', 'B']), $bundle);
+        $this->assertEquals(['A' => 1], $remainingBooks);
+    }
+
+    public function testAMaximumCardinalityOfABundleCanBeRequested()
+    {
+        list ($bundle, $remainingBooks) = Bundle::extractGreedily(['A' => 1, 'B' => 1, 'C' => 1], 2);
+        $this->assertEquals(new Bundle(['A', 'B']), $bundle);
+        $this->assertEquals(['C' => 1], $remainingBooks);
+    }
+
     public function testAllPossibleBundlesOfSingleBooksCanBeRequested()
     {
         $all = Bundle::extractAll(['A' => 1, 'B' => 1, 'C' => 1], 1);
@@ -255,6 +281,26 @@ class Cart
         
         return $bags->minimumBag();
     }
+
+    private function greedyBundles()
+    {
+        $bundles = [];
+        $remainingBooks = $this->books;
+        while ($remainingBooks) {
+            list ($bundle, $remainingBooks) = Bundle::extractGreedily($remainingBooks);
+            $bundles[] = $bundle;
+        }
+        return $bundles;
+    }
+
+    private function sumOf(array $bundles)
+    {
+        $price = 0;
+        foreach ($bundles as $bundle) {
+            $price += $bundle->price();
+        }
+        return $price;
+    }
 }
 
 class Bundle implements Countable
@@ -271,6 +317,24 @@ class Bundle implements Countable
     private $string;
     private static $bundlesMemoization;
     private static $cache = [];
+
+    public static function extractGreedily(array $books, $maximumCardinality = 5)
+    {
+        $titles = [];
+        $extracted = 0;
+        foreach ($books as $title => $number) {
+            $books[$title]--;
+            if ($books[$title] == 0) {
+                unset($books[$title]);
+            }
+            $titles[] = $title;
+            $extracted++;
+            if ($extracted == $maximumCardinality) {
+                break;
+            }
+        }
+        return [new self($titles), $books];
+    }
 
     public static function extractAllUpTo(array $books, $maximumCardinality)
     {
